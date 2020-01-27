c7b6efa950a2722bdf0f5595f4f934b2e82c8045
Вячеслав Чумаков <v.a.chumakov@gmail.com>
2020-01-27 12:20:36 +0400

    creat_repsit_mvc

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return 'Main page';
+});
+Route::get('/post/', function () {
+    return 'Post page';
+});
+Route::get('/post/1/', function () {
+    return 'First page';
+});
+Route::get('/post/{address}', function ($address) {
+    return 'Post'.$address;
+});
